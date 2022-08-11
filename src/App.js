486ae97b3c5f8bfb43bc837b5f295f4d30edcdc2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-11 23:36:17 +0900

    #33 Adding routing to App 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from "react-router-dom"
 
 function App() {
   return (
