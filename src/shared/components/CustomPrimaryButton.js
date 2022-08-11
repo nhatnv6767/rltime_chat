2d977a5ad198df61c12baf44819e3e8b798982e7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 01:23:26 +0900

    #37 Creating custom button 5

--- src/shared/components/CustomPrimaryButton.js
+++ src/shared/components/CustomPrimaryButton.js
@@ -8,9 +8,21 @@ const CustomPrimaryButton = ({
                                  onClick,
                              }) => {
     return (
-        <div>
-
-        </div>
+        <Button
+            variant="contained"
+            sx={{
+                bgcolor: "#5865F2",
+                color: "white",
+                textTransform: "none",
+                fontSize: "16px",
+                fontWeight: 500,
+                width: "100%",
+                height: "40px",
+            }}
+            style={additionalStyles ? additionalStyles : {}}
+        >
+            {label}
+        </Button>
     );
 };
 
