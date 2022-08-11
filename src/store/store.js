ae315decfa1889f0495091533ae79623fc39cbef
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-11 21:10:24 +0900

    #32 Configuring Redux Store 3

--- src/store/store.js
+++ src/store/store.js
@@ -1,2 +1,9 @@
 import {composeWithDevTools} from "redux-devtools-extension";
 import {combineReducers, createStore, applyMiddleware} from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({
+
+})
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
