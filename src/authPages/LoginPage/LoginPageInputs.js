4e1463a8ab4539b13d55eb61847e1fa3574692e4
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 01:11:20 +0900

    #36 Creating custom input component 12

--- src/authPages/LoginPage/LoginPageInputs.js
+++ src/authPages/LoginPage/LoginPageInputs.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import InputWithLabel from "../../shared/components/InputWithLabel";
 
-const LoginPageInputs = () => {
+const LoginPageInputs = ({mail, setMail, password, setPassword}) => {
     return (
         <>
             <InputWithLabel
-                value
-                setValue
+                value={mail}
+                setValue={setMail}
                 label="E-mail"
                 type="text"
                 placeholder="Enter e-mail address"
+            />
+            <InputWithLabel
+                value={password}
+                setValue={setPassword}
+                label="Password"
+                type="password"
+                placeholder="Enter password"
             />
         </>
     );
