3f287b25552445fb2bd1b3346af5dc3f4ce22370
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 00:59:47 +0900

    #36 Creating custom input component 5

--- src/shared/components/InputWithLabel.js
+++ src/shared/components/InputWithLabel.js
@@ -2,8 +2,17 @@ import React from 'react';
 import {styled} from "@mui/material/styles"
 
 const Wrapper = styled("div")({
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    width: "100%",
+})
+
+const Label = styled("p")({
     color: "#b9bbbe",
     textTransform: "uppercase",
+    fontWeight: "600",
+    fontSize: "16px",
 })
 
 const InputWithLabel = (props) => {
