99c292bd7d6cb504c5ec65b12dea384cf7c498c6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 01:38:20 +0900

    #38 Creating Redirect component 5

--- src/authPages/LoginPage/LoginPageFooter.js
+++ src/authPages/LoginPage/LoginPageFooter.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
+import RedirectInfo from "../../shared/components/RedirectInfo";
 
 const LoginPageFooter = ({handleLogin, isFormValid}) => {
     return (
-        <div>
-            <CustomPrimaryButton
-                label="Log in"
-                additionalStyles={{marginTop: "30px"}}
-                disabled={!isFormValid}
-                onClick={handleLogin}
-            />
-        </div>
+        <>
+            <div>
+                <CustomPrimaryButton
+                    label="Log in"
+                    additionalStyles={{marginTop: "30px"}}
+                    disabled={!isFormValid}
+                    onClick={handleLogin}
+                />
+            </div>
+            <RedirectInfo/>
+        </>
     );
 };
 
