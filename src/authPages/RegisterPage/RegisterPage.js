31091acb2c46879e2475172bc73ff8ffaad16d8a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 00:35:13 +0900

    #34 Creating Authentication Box Component with MaterialUI 7

--- src/authPages/RegisterPage/RegisterPage.js
+++ src/authPages/RegisterPage/RegisterPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import AuthBox from "../../shared/components/AuthBox";
 
 const RegisterPage = () => {
     return (
-        <div>
+        <AuthBox>
             Register Page
-        </div>
+        </AuthBox>
     );
 };
 
