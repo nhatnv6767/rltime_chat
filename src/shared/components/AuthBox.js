4b2dcbd82213ca20b296531e676664510291f7c1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 00:24:30 +0900

    #34 Creating Authentication Box Component with MaterialUI 2

--- src/shared/components/AuthBox.js
+++ src/shared/components/AuthBox.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Box from "@mui/material/Box"
+import {styled} from "@mui/material/styles"
+
+const BoxWrapper = styled("div")({
+
+})
 
 const AuthBox = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
