512b3efcfa89a379713c16cba1b026ebdf3a23f7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 21:26:23 +0900

    #42 Register Page 3

---
+++ src/authPages/RegisterPage/RegisterPageInputs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegisterPageInputs = (props) => {
+    const {mail, setMail, username, setUsername, password, setPassword} = props;
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default RegisterPageInputs;
