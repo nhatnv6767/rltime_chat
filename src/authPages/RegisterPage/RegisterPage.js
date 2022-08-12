54f9edbbf4641d92567301908a4f24a1bf91e338
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 21:47:02 +0900

    #42 Register Page 11

--- src/authPages/RegisterPage/RegisterPage.js
+++ src/authPages/RegisterPage/RegisterPage.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import AuthBox from "../../shared/components/AuthBox";
 import {Typography} from "@mui/material";
 import RegisterPageInputs from "./RegisterPageInputs";
+import RegisterPageFooter from "./RegisterPageFooter";
 
 const RegisterPage = () => {
     const [mail, setMail] = useState("");
@@ -9,6 +10,13 @@ const RegisterPage = () => {
     const [password, setPassword] = useState("");
 
     const [isFormValid, setIsFormValid] = useState(false);
+
+    const handleRegister = () => {
+        console.log(mail);
+        console.log(username);
+        console.log(password);
+        console.log("Registering");
+    }
     return (
         <AuthBox>
             <Typography variant="h5" sx={{color: 'white'}}>
@@ -21,6 +29,10 @@ const RegisterPage = () => {
                 setUsername={setUsername}
                 password={password}
                 setPassword={setPassword}
+            />
+            <RegisterPageFooter
+                handleRegister={handleRegister}
+                isFormValid={isFormValid}
             />
         </AuthBox>
     );
