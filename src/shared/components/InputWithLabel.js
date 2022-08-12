4672be20e13ef064577fec062480009b5819f935
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 21:36:07 +0900

    #42 Register Page 5.fix

--- src/shared/components/InputWithLabel.js
+++ src/shared/components/InputWithLabel.js
@@ -1,37 +1,37 @@
 import React from 'react';
-import {styled} from "@mui/material/styles"
+import {styled} from "@mui/material/styles";
 
 const Wrapper = styled("div")({
     display: "flex",
     justifyContent: "center",
     flexDirection: "column",
     width: "100%",
-})
+});
 
 const Label = styled("p")({
     color: "#b9bbbe",
     textTransform: "uppercase",
     fontWeight: "600",
     fontSize: "16px",
-})
+});
 
 const Input = styled("input")({
     flexGrow: 1,
     height: "40px",
     border: "1px solid black",
-    borderRadius:"5px",
+    borderRadius: "5px",
     color: "#dcddde",
     background: "#35393f",
     margin: 0,
     fontSize: "16px",
     padding: "0 5px"
-})
+});
 
 const InputWithLabel = (props) => {
-    const {value, setValue, label, type, placehoder} = props;
+    const {value, setValue, label, type, placeholder} = props;
     const handleValueChange = (event) => {
         setValue(event.target.value);
-    }
+    };
     return (
         <Wrapper>
             <Label>
@@ -41,7 +41,7 @@ const InputWithLabel = (props) => {
                 value={value}
                 onChange={handleValueChange}
                 type={type}
-                placehoder={placehoder}
+                placeholder={placeholder}
             />
         </Wrapper>
     );
