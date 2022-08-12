cd9f7e3247bd9474d5e03f0b93b37aaa1af110dd
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 21:39:50 +0900

    #42 Register Page 8

--- src/authPages/RegisterPage/RegisterPageFooter.js
+++ src/authPages/RegisterPage/RegisterPageFooter.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
+import RedirectInfo from "../../shared/components/RedirectInfo";
+import {useNavigate} from "react-router-dom";
+import {Tooltip} from "@mui/material";
 
-const RegisterPageFooter = () => {
+const getFormNotValidMessage = () => {
+    return "Enter correct e-mail address and password should contains between 6 and 12 characters";
+};
+const getFormValidMessage = () => {
+    return "Press to log in";
+};
+
+const RegisterPageFooter = ({handleLogin, isFormValid}) => {
+
+    const history = useNavigate();
+    const handlePushToRegisterPage = () => {
+        history("/register");
+    };
+
+
     return (
-        <div>
-
-        </div>
+        <>
+            <Tooltip
+                title={!isFormValid ? getFormNotValidMessage() : getFormValidMessage()}
+            >
+                <div>
+                    <CustomPrimaryButton
+                        label="Log in"
+                        additionalStyles={{marginTop: "30px"}}
+                        disabled={!isFormValid}
+                        onClick={handleLogin}
+                    />
+                </div>
+            </Tooltip>
+            <RedirectInfo
+                text="Need an account? "
+                redirectText="Create an account"
+                additionalStyles={{marginTop: "5px"}}
+                redirectHandler={handlePushToRegisterPage}
+            />
+        </>
     );
 };
 
