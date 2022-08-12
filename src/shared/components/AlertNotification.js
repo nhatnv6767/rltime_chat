9473ab0ae061e8ac6e1ad09926ca7e2d5a1827d6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-13 00:19:44 +0900

    #47 Custom Alert 2

--- src/shared/components/AlertNotification.js
+++ src/shared/components/AlertNotification.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import Alert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
+import {connect} from 'react-redux';
 
-const AlertNotification = () => {
+const AlertNotification = (props) => {
     return (
         <div>
 
