4558e9e6e5f72cb1ba82205de632fd4e0c9604d7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 21:21:49 +0900

    #42 Register Page 1

--- src/authPages/RegisterPage/RegisterPage.js
+++ src/authPages/RegisterPage/RegisterPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AuthBox from "../../shared/components/AuthBox";
+import {Typography} from "@mui/material";
 
 const RegisterPage = () => {
     return (
         <AuthBox>
-            Register Page
+            <Typography variant="5" sx={{color: 'white'}}>
+                Create an account
+            </Typography>
         </AuthBox>
     );
 };
