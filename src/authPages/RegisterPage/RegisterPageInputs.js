c46d292762a9f1409c0b1e0ea3996df38f2738a3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 21:30:33 +0900

    #42 Register Page 5

--- src/authPages/RegisterPage/RegisterPageInputs.js
+++ src/authPages/RegisterPage/RegisterPageInputs.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import InputWithLabel from "../../shared/components/InputWithLabel";
 
 const RegisterPageInputs = (props) => {
     const {mail, setMail, username, setUsername, password, setPassword} = props;
     return (
-        <div>
-
-        </div>
+        <>
+            <InputWithLabel
+                value={mail}
+                setValue={setMail}
+                label="E-mail address"
+                type="text"
+                placeholder="Enter email address"
+            />
+            <InputWithLabel
+                value={username}
+                setValue={setUsername}
+                label="Username"
+                type="text"
+                placeholder="Enter an username"
+            />
+            <InputWithLabel
+                value={password}
+                setValue={setPassword}
+                label="Password"
+                type="password"
+                placeholder="Enter password"
+            />
+        </>
     );
 };
 
