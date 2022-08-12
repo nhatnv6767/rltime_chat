fa5f035184f0df64ab6a1d4e2be2d54c3b42f2cc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 23:15:00 +0900

    #44 Authentication action 9

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -6,9 +6,11 @@ const initState = {
 
 const reducer = (state = initState, action) => {
     switch (action.type) {
-        case "DUMMY":
+        case authActions.SET_USER_DETAILS:
             return {
                 ...state,
+                // because we passing them in setUserDetails(authActions)
+                userDetails: action.userDetails
             };
         default:
             return state;
