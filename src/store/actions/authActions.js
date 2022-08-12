3ebfa8e4eeb3040a43738836d86ff555ab96a753
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-12 23:04:02 +0900

    #44 Authentication action 2

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,9 +1,11 @@
-import * as api from '../../api'
+import * as api from '../../api';
 
 const authActions = {
     SET_USER_DETAILS: 'AUTH.SET_USER_DETAILS',
-}
+};
 
 export const getActions = (dispatch) => {
-    return {}
-}
+    return {
+        login: (userDetails, history) => dispatch(login(userDetails, history))
+    };
+};
