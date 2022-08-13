8267a7c670d9cc27bada227cd3eb28615b47a300
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-13 21:29:10 +0900

    #48 Store for Alert Message 4

--- src/store/actions/alertActions.js
+++ src/store/actions/alertActions.js
@@ -2,3 +2,12 @@ const alertActions = {
     SHOW_ALERT_MESSAGE: "ALERT.OPEN_ALERT_MESSAGE",
     CLOSE_ALERT_MESSAGE: "ALERT.CLOSE_ALERT_MESSAGE",
 };
+
+export const getActions = (dispatch) => {
+    return {
+        openAlertMessage: (content) => dispatch(openAlertMessage(content)),
+        closeAlertMessage: () =>
+            dispatch(closeAlertMessage())
+
+    };
+};
