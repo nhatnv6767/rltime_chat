dd719c77baa7d3b66dc369dfd38c2d75c17c6b2b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-13 21:24:28 +0900

    #48 Store for Alert Message 2

--- src/store/reducers/alertReducer.js
+++ src/store/reducers/alertReducer.js
@@ -2,3 +2,17 @@ const initState = {
     showAlertMessage: false,
     alertMessageContent: null,
 };
+
+const reducer = (state = initState, action) => {
+    switch (action.type) {
+        case "DUMMY":
+            return {
+                ...state,
+            };
+
+        default:
+            return state;
+    }
+};
+
+export default reducer;
