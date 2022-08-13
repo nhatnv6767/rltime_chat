04c6403493abab176612a3fd2ece2863c8f9c69e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-14 02:14:57 +0900

    #49 Display server messages 1

--- src/shared/components/AlertNotification.js
+++ src/shared/components/AlertNotification.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Alert from "@mui/material/Alert";
 import Snackbar from "@mui/material/Snackbar";
 import {connect} from 'react-redux';
+import {getActions} from "../../store/actions/alertActions";
 
 const AlertNotification = (props) => {
     return (
