577f25fb709fca6c37b5bc03c72b65a330cc9582
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-14 23:06:01 +0900

    #54 Creating Dashboard Structure 1

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {styled} from "@mui/material/styles";
+
+const Wrapper = styled("div")({
+    width: "100%",
+    height: "100vh",
+    display: "flex",
+});
 
 const Dashboard = () => {
     return (
-        <div>
+        <Wrapper>
             Dashboard
-        </div>
+        </Wrapper>
     );
 };
 
