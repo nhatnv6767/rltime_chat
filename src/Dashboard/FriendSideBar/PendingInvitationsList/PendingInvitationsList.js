8782c1e080f2425d0e772761ae7f4f7ca787ebaf
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-15 03:42:08 +0900

    #57 Sidebar 8

--- src/Dashboard/FriendSideBar/PendingInvitationsList/PendingInvitationsList.js
+++ src/Dashboard/FriendSideBar/PendingInvitationsList/PendingInvitationsList.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {styled} from "@mui/material/styles";
+
+
+const MainContainer = styled("div")({
+    width: "100%",
+    height: "22%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    overflow: "auto",
+});
 
 const PendingInvitationsList = () => {
     return (
-        <div>
-
-        </div>
+        <MainContainer>
+        </MainContainer>
     );
 };
 
