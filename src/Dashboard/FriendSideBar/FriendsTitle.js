919d86377d7713f6fd4ec55f88ee9b0963d410fc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-15 00:49:34 +0900

    #57 Sidebar 3

--- src/Dashboard/FriendSideBar/FriendsTitle.js
+++ src/Dashboard/FriendSideBar/FriendsTitle.js
@@ -3,7 +3,14 @@ import {Typography} from "@mui/material";
 
 const FriendsTitle = () => {
     return (
-        <Typography>
+        <Typography
+            sx={{
+                textTransform: "uppercase",
+                color: "#8e9297",
+                fontSize: "14px",
+                marginTop: "10px",
+            }}
+        >
         </Typography>
     );
 };
