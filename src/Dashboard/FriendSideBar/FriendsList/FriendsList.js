6bd48e91f218d563437ebd1e3dfa813135789def
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-15 03:39:38 +0900

    #57 Sidebar 7

--- src/Dashboard/FriendSideBar/FriendsList/FriendsList.js
+++ src/Dashboard/FriendSideBar/FriendsList/FriendsList.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {styled} from "@mui/material/styles";
+
+const MainContainer = styled("div")({
+    /* Telling the component to take up all the space it can. */
+    flexGrow: 1,
+    width: "100%",
+
+})
 
 const FriendsList = () => {
     return (
-        <div>
+        <MainContainer>
 
-        </div>
+        </MainContainer>
     );
 };
 
