d54bd7004c4661dce602b05db22febff2f7ba7a2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-14 23:45:25 +0900

    #54 Creating Dashboard Structure 4

--- src/Dashboard/SideBar/SideBar.js
+++ src/Dashboard/SideBar/SideBar.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {styled} from "@mui/material/styles";
+
+const MainContainer = styled("div")({
+    width: "72px",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#202225"
+})
 
 const SideBar = () => {
     return (
-        <div>
-            
-        </div>
+        <MainContainer>
+
+        </MainContainer>
     );
 };
 
