7f9197e8174df3ccca0bc428f7669b0c164be3ba
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-15 00:43:47 +0900

    #55 Creating Add Friend Button 3

--- src/Dashboard/FriendSideBar/AddFriendButton.js
+++ src/Dashboard/FriendSideBar/AddFriendButton.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
 
+const additionalStyles = {
+    marginTop: "10px",
+    marginLeft: "5px",
+    width: "80%",
+    height: "30px",
+    background: "#3ba55d"
+};
+
 const AddFriendButton = () => {
+    const handleOpenAddFriendDialog = () => {
+    };
     return (
         <>
-            <CustomPrimaryButton/>
+            <CustomPrimaryButton
+                additionalStyles={additionalStyles}
+                label="Add Friend"
+                onClick={handleOpenAddFriendDialog}
+            />
         </>
     );
 };
