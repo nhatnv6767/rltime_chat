20b15e45103ac3a43019ed45c0bf3f8634b4dba5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-15 00:34:52 +0900

    #55 Creating Main Page Button 5

--- src/Dashboard/SideBar/MainPageButton.js
+++ src/Dashboard/SideBar/MainPageButton.js
@@ -4,7 +4,19 @@ import GroupsIcon from "@mui/icons-material/Groups";
 
 const MainPageButton = () => {
     return (
-        <Button>
+        <Button
+            style={{
+                width: "48px",
+                height: "48px",
+                borderRadius: "16px",
+                margin: 0,
+                padding: 0,
+                minWidth: 0,
+                marginTop: "10px",
+                color: "white",
+                backgroundColor: "#5865F2"
+            }}
+        >
             <GroupsIcon/>
         </Button>
     );
