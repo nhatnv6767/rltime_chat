acc2a5f1d9228f9845ed7bcefca0fd0c8a633ed4
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-17 00:04:39 +0900

    #69 Connecting to SocketIO Server from Client Side 2

---
+++ src/realtimeCommunication/socketConnection.js
@@ -0,0 +1,11 @@
+import io from 'socket.io-client';
+
+let socket = null;
+export const connectWithSocketServer = (userDetails) => {
+  socket = io('http://localhost:5002');
+
+  socket.on('connect', () => {
+    console.log('Successfully connected with socket.io server');
+    console.log(socket.id);
+  });
+};
