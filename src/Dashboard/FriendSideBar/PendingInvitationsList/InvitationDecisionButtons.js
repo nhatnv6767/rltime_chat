8318c6a6c075dbc05b383dad9d6bd7660415564b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-16 21:06:58 +0900

    #63 Invitations List - Decision Buttons 4

--- src/Dashboard/FriendSideBar/PendingInvitationsList/InvitationDecisionButtons.js
+++ src/Dashboard/FriendSideBar/PendingInvitationsList/InvitationDecisionButtons.js
@@ -4,13 +4,16 @@ import ClearIcon from "@mui/icons-material/Clear";
 import {Box, IconButton} from "@mui/material";
 import {styled} from "@mui/material/styles";
 
-const InvitationDecisionButtons = ({disabled}) => {
+const InvitationDecisionButtons = ({disabled, acceptInvitationHandler}) => {
     return (
         <Box sx={{display: 'flex'}}>
             <IconButton
                 style={{color: 'white'}}
                 disabled={disabled}
-            />
+                onClick={acceptInvitationHandler}
+            >
+                <CheckIcon/>
+            </IconButton>
         </Box>
     );
 };
