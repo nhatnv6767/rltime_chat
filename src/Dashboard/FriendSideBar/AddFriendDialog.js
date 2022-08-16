75774666db7dc7517b593114605f6f9f0d1b120f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-16 14:03:16 +0900

    #58 Add Friend Dialog 14

--- src/Dashboard/FriendSideBar/AddFriendDialog.js
+++ src/Dashboard/FriendSideBar/AddFriendDialog.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {validateMail} from "../../shared/utils/validators";
-import {Dialog} from "@mui/material/Dialog";
+import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
