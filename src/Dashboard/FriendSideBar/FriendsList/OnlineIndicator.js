2f9fa9f86f6ed8a349bbc509c8971ba4e1a633f6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-16 17:47:57 +0900

    #61 Online Indicator 2

--- src/Dashboard/FriendSideBar/FriendsList/OnlineIndicator.js
+++ src/Dashboard/FriendSideBar/FriendsList/OnlineIndicator.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import {Box} from '@mui/material';
 
 const OnlineIndicator = () => {
     return (
-        <div>
+        <Box>
 
-        </div>
+        </Box>
     );
 };
 
