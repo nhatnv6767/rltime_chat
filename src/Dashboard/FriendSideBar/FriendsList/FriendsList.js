17584e1249a8502db5c379595a7cf6947c374ece
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-16 17:17:29 +0900

    #60 Preparing Friends List 1

--- src/Dashboard/FriendSideBar/FriendsList/FriendsList.js
+++ src/Dashboard/FriendSideBar/FriendsList/FriendsList.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import {styled} from "@mui/material/styles";
 
+const DUMMY_FRIENDS = [
+    {
+        id: 1,
+        username: "Mark",
+        isOnline: true,
+    },
+    {
+        id: 2,
+        username: "Anna",
+        isOnline: false,
+    },
+    {
+        id: 3,
+        username: "John",
+        isOnline: false,
+    },
+];
+
 const MainContainer = styled("div")({
     /* Telling the component to take up all the space it can. */
     flexGrow: 1,
     width: "100%",
 
-})
+});
 
 const FriendsList = () => {
     return (
