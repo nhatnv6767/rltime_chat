ef8d67c69a3e34e0a8540c64005192f037dd6242
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-16 22:41:16 +0900

    #65 Checking if token exists on Dashboard Page 2

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,33 +1,36 @@
 import React, {useEffect} from 'react';
-import {styled} from "@mui/material/styles";
-import SideBar from "./SideBar/SideBar";
-import FriendSideBar from "./FriendSideBar/FriendSideBar";
-import Messenger from "./Messenger/Messenger";
-import AppBar from "./AppBar/AppBar";
+import {styled} from '@mui/material/styles';
+import SideBar from './SideBar/SideBar';
+import FriendSideBar from './FriendSideBar/FriendSideBar';
+import Messenger from './Messenger/Messenger';
+import AppBar from './AppBar/AppBar';
+import {logout} from '../shared/utils/auth';
 
-const Wrapper = styled("div")({
-    width: "100%",
-    height: "100vh",
-    display: "flex",
+const Wrapper = styled('div')({
+  width: '100%',
+  height: '100vh',
+  display: 'flex',
 });
 
 const Dashboard = () => {
 
-    useEffect(() => {
-        const userDetails = localStorage.getItem("user");
-        if (!userDetails) {
-            window.location.pathname = "login";
-        }
-    }, []);
+  useEffect(() => {
+    const userDetails = localStorage.getItem('user');
+    if (!userDetails) {
+      logout();
+    } else {
+      setUserDetails(JSON.parse(userDetails));
+    }
+  }, []);
 
-    return (
-        <Wrapper>
-            <SideBar/>
-            <FriendSideBar/>
-            <Messenger/>
-            <AppBar/>
-        </Wrapper>
-    );
+  return (
+      <Wrapper>
+        <SideBar/>
+        <FriendSideBar/>
+        <Messenger/>
+        <AppBar/>
+      </Wrapper>
+  );
 };
 
 export default Dashboard;
