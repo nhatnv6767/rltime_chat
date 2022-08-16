4436a1851197dd42499275937f98bf0280fec51f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-17 00:06:35 +0900

    #69 Connecting to SocketIO Server from Client Side 3

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -7,6 +7,9 @@ import AppBar from './AppBar/AppBar';
 import {logout} from '../shared/utils/auth';
 import {connect} from 'react-redux';
 import {getActions} from '../store/actions/authActions';
+import {
+  connectWithSocketServer,
+} from '../realtimeCommunication/socketConnection';
 
 const Wrapper = styled('div')({
   width: '100%', height: '100vh', display: 'flex',
@@ -19,6 +22,7 @@ const Dashboard = ({setUserDetails}) => {
       logout();
     } else {
       setUserDetails(JSON.parse(userDetails));
+      connectWithSocketServer();
     }
   }, []);
 
