64e465394b47671490f7ba14569a51d8a2c9d7b2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-16 18:09:54 +0900

    #62 Creating Dummy Invitations List 4

--- src/Dashboard/FriendSideBar/PendingInvitationsList/PendingInvitationsListItem.js
+++ src/Dashboard/FriendSideBar/PendingInvitationsList/PendingInvitationsListItem.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const PendingInvitationsListItem = () => {
+const PendingInvitationsListItem = ({
+                                        id,
+                                        username,
+                                        mail,
+                                        acceptFriendInvitation = () => {
+                                        },
+                                        rejectFriendInvitation = () => {
+                                        },
+
+                                    }) => {
+    const [buttonsDisabled, setButtonsDisabled] = useState(false);
+    const handleAcceptInvitaion = () => {
+        acceptFriendInvitation({id: id})
+
+    }
     return (
         <div>
 
