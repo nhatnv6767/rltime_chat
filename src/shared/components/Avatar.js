0fca1939d3d96542f748bd7b33e53464e4e656ff
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-16 17:30:25 +0900

    #60 Preparing Friends List 9

--- src/shared/components/Avatar.js
+++ src/shared/components/Avatar.js
@@ -1,4 +1,19 @@
 import React from 'react';
+import {styled} from "@mui/material/styles";
+
+const AvatarPreview = styled("div")({
+    height: "42px",
+    width: "42px",
+    backgroundColor: "#5865f2",
+    borderRadius: "42px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "20px",
+    fontWeight: "700",
+    marginLeft: "5px",
+    color: "white"
+});
 
 const Avatar = ({username, large}) => {
     return (
