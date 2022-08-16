8bd088048aecb220e6b1866c39c4f6f97bbe30e4
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-16 17:34:31 +0900

    #60 Preparing Friends List 11

--- src/shared/components/Avatar.js
+++ src/shared/components/Avatar.js
@@ -17,7 +17,9 @@ const AvatarPreview = styled("div")({
 
 const Avatar = ({username, large}) => {
     return (
-        <AvatarPreview>
+        <AvatarPreview
+            style={large ? {height: "80px", width: "80px"} : {}}
+        >
             {username.substring(0, 2)}
         </AvatarPreview>
     );
