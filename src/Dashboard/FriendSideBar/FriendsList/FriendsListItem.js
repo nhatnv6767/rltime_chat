b3623a9b4e9fe91b205e579bb17361053cd18a18
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-16 17:23:45 +0900

    #60 Preparing Friends List 5

--- src/Dashboard/FriendSideBar/FriendsList/FriendsListItem.js
+++ src/Dashboard/FriendSideBar/FriendsList/FriendsListItem.js
@@ -4,6 +4,17 @@ import Button from '@mui/material/Button';
 const FriendsListItem = ({id, username, isOnline}) => {
     return (
         <Button
+            style={{
+                width: "100%",
+                height: "42px",
+                marginTop: "10px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-start",
+                textTransform: "none",
+                color: "black",
+                position: "relative",
+            }}
         >
 
         </Button>
