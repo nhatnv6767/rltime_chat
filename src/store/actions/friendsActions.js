aab3c9404feb195f479d8669e4d3620d516365cf
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-17 18:16:28 +0900

    #76 Preparing action to send friend invitation 1

--- src/store/actions/friendsActions.js
+++ src/store/actions/friendsActions.js
@@ -3,3 +3,10 @@ export const friendsActions = {
     SET_PENDING_FRIENDS_INVITATIONS: "FRIENDS.SET_PENDING_FRIENDS_INVITATIONS",
     SET_ONLINE_USERS: "FRIENDS.SET_ONLINE_USERS",
 }
+
+export const getActions = (dispatch) => {
+    return {
+        sendFriendInvitaion: (data, closeDialogHandler) =>
+            dispatch(sendFriendInvitaion(data, closeDialogHandler))
+    }
+}
