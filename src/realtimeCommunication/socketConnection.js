79e79847649fb682f8583e45f7aaa5753d96a95a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 00:13:04 +0900

    #85 Preparing SocketIO Client Event Listeners for realtime friends invitations 3

--- src/realtimeCommunication/socketConnection.js
+++ src/realtimeCommunication/socketConnection.js
@@ -1,22 +1,23 @@
 import io from 'socket.io-client';
 import store from "../store/store";
+import {setPendingFriendsInvitations} from "../store/actions/friendsActions";
 
 let socket = null;
 export const connectWithSocketServer = (userDetails) => {
-  const jwtToken = userDetails.token;
-  socket = io('http://localhost:5002', {
-    auth: {
-      token: jwtToken,
-    },
-  });
+    const jwtToken = userDetails.token;
+    socket = io('http://localhost:5002', {
+        auth: {
+            token: jwtToken,
+        },
+    });
 
-  socket.on('connect', () => {
-    console.log('Successfully connected with socket.io server');
-    console.log(socket.id);
-  });
+    socket.on('connect', () => {
+        console.log('Successfully connected with socket.io server');
+        console.log(socket.id);
+    });
 
-  socket.on("friends-invitations", (data) => {
-    const {pendingInvitations} = data;
-    store.dispatch(setPendingFriendsInvitations())
-  })
+    socket.on("friends-invitations", (data) => {
+        const {pendingInvitations} = data;
+        store.dispatch(setPendingFriendsInvitations(pendingInvitations))
+    })
 };
