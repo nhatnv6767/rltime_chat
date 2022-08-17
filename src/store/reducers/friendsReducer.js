fa80126d36b78dc0dbfb7961cef34f979a9b85cb
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-17 18:12:27 +0900

    #75 Redux - Preparing actions and reducers for friends logic 3

--- src/store/reducers/friendsReducer.js
+++ src/store/reducers/friendsReducer.js
@@ -1,3 +1,5 @@
+import {friendsActions} from "../actions/friendsActions"
+
 const initState = {
     friends: [],
     pendingFriendsInvitations: [],
@@ -6,7 +8,23 @@ const initState = {
 
 const reducer = (state = initState, action) => {
     switch (action.type) {
-
+        case friendsActions.SET_PENDING_FRIENDS_INVITATIONS:
+            return {
+                ...state,
+                pendingFriendsInvitations: action.pendingFriendsInvitations
+            }
+        case friendsActions.SET_FRIENDS:
+            return {
+                ...state,
+                friends: action.friends
+            }
+        case friendsActions.SET_ONLINE_USERS:
+            return {
+                ...state,
+                onlineUsers: action.onlineUsers
+            }
+        default:
+            return state;
     }
 }
 
