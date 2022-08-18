1eb21aff537d125646eb645b7004d0293ae3ea36
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 20:03:13 +0900

    #107 Redux Store for chat functionality 1

---
+++ src/store/actions/chatActions.js
@@ -0,0 +1,10 @@
+export const chatTypes = {
+    DIRECT: "DIRECT",
+    GROUP: "GROUP",
+}
+
+export const chatActions = {
+    SET_CHOSEN_CHAT_DETAILS: "CHAT.SET_CHOSEN_CHAT_DETAILS",
+    SET_MESSAGES: "CHAT.SET_MESSAGES",
+    SET_CHAT_TYPE: "CHAT.SET_CHAT_TYPE",
+}
