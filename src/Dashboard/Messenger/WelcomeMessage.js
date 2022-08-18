add266b9b71bad7b8192a4663b6637110b306b22
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 23:16:46 +0900

    #110 Creating Messenger UI 4

--- src/Dashboard/Messenger/WelcomeMessage.js
+++ src/Dashboard/Messenger/WelcomeMessage.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Typography} from "@mui/material";
+import {styled} from "@mui/material/styles";
+
+const Wrapper = styled("div")({
+    flexGrow: 1,
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+})
 
 const WelcomeMessage = () => {
     return (
-        <div>
-
-        </div>
+        <Wrapper>
+            <Typography>To start chatting - choose conversation</Typography>
+        </Wrapper>
     );
 };
 
