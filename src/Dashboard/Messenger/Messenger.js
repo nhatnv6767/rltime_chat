94b4361778ddfc88e9f9bd93027b85dccac4cc0c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 23:13:04 +0900

    #110 Creating Messenger UI 3

--- src/Dashboard/Messenger/Messenger.js
+++ src/Dashboard/Messenger/Messenger.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {styled} from "@mui/material/styles";
 import {connect} from 'react-redux'
+import WelcomeMessage from "./WelcomeMessage";
+import MessengerContent from "./MessengerContent";
 
 const MainContainer = styled("div")({
     flexGrow: 1,
@@ -12,7 +14,14 @@ const MainContainer = styled("div")({
 const Messenger = ({chosenChatDetails}) => {
     return (
         <MainContainer>
-            {!chosenChatDetails ? (<WelcomeMessage/>) : (<MessengerContent/>)}
+            {!chosenChatDetails
+                ? (<WelcomeMessage/>)
+                : (
+                    <MessengerContent
+                        chosenChatDetails={chosenChatDetails}
+                    />
+                )
+            }
         </MainContainer>
     );
 };
