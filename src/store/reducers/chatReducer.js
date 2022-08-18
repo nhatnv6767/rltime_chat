5de8e69e2505057023b20d46e82bbde9d804d4e0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 20:08:31 +0900

    #107 Redux Store for chat functionality 4

---
+++ src/store/reducers/chatReducer.js
@@ -0,0 +1,13 @@
+import chatActions from "../actions/chatActions"
+
+const initState = {
+    chosenChatDetails: null,
+    chatType: null,
+    messages: []
+}
+
+const reducer = (state = initState, action) => {
+
+}
+
+export default reducer;
