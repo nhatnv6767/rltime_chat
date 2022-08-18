ee6b70dbdcabedc5ff6e3ee788358d0bacf8ef6b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 00:13:15 +0900

    #112 Rendering single messages 7

--- src/Dashboard/Messenger/Messages/Message.js
+++ src/Dashboard/Messenger/Messages/Message.js
@@ -51,7 +51,13 @@ const Message = ({content, sameAuthor, username, date, sameDay}) => {
                         color: "white",
                     }}
                 >
-                    {username}
+                    {username}{" "}
+                    <span
+                        style={{
+                            fontSize: "12px",
+                            color: "#72767d"
+                        }}
+                    >{date}</span>
                 </Typography>
             </MainContainer>
         </MainContainer>
