068001a359b92cf4af356ab12de5834d5c06a8b0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 00:04:47 +0900

    #112 Rendering single messages 4

--- src/Dashboard/Messenger/Messages/Message.js
+++ src/Dashboard/Messenger/Messages/Message.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {styled} from "@mui/material/styles";
+import Avatar from "../../../shared/components/Avatar";
+import {Typography} from "@mui/material";
+
+const MainContainer = styled("div")({
+    width: "97%",
+    display: "flex",
+    marginTop: "10px",
+})
 
 const Message = () => {
     return (
-        <div>
-
-        </div>
+        <MainContainer>
+        </MainContainer>
     );
 };
 
