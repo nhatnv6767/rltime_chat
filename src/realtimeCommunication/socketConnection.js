e9fd8152cb3aeb3f457274dc93f5b2091fc837f7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 10:15:11 +0900

    #87 Emitting events to clients of specific id 10

--- src/realtimeCommunication/socketConnection.js
+++ src/realtimeCommunication/socketConnection.js
@@ -18,6 +18,8 @@ export const connectWithSocketServer = (userDetails) => {
 
     socket.on("friends-invitations", (data) => {
         const {pendingInvitations} = data;
+        console.log("friends invitations event  came")
+        console.log(pendingInvitations)
         store.dispatch(setPendingFriendsInvitations(pendingInvitations))
     })
 };
