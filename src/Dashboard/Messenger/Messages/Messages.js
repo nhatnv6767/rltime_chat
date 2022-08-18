006e375b77c9d86d28711df22b7b589d1220d1f9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 23:57:39 +0900

    #112 Rendering single messages 2

--- src/Dashboard/Messenger/Messages/Messages.js
+++ src/Dashboard/Messenger/Messages/Messages.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {styled} from "@mui/material/styles";
 import MessagesHeader from "./MessagesHeader"
 import {connect} from "react-redux"
+import DUMMY_MESSAGES from "./DUMMY_MESSAGES"
 
 const MainContainer = styled("div")({
     height: "calc(100% - 60px)",
@@ -17,6 +18,11 @@ const Messages = ({chosenChatDetails, messages}) => {
             <MessagesHeader
                 name={chosenChatDetails?.name}
             />
+            {
+                DUMMY_MESSAGES.map((messages, index) => {
+                    return <Message/>
+                })
+            }
         </MainContainer>
     );
 };
