a568f75413b84e0a4f17414e871f49a363ad8f2d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 23:46:47 +0900

    #111 Creating Message Header 4

--- src/Dashboard/Messenger/Messages/MessagesHeader.js
+++ src/Dashboard/Messenger/Messages/MessagesHeader.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import {styled} from "@mui/material/styles";
+import Avatar from "../../../shared/components/Avatar";
+import {Typography} from "@mui/material";
 
-const MessagesHeader = () => {
+const MainContainer = styled("div")({
+    width: "98%",
+    display: "column",
+    marginTop: "10px",
+})
+
+const MessagesHeader = ({name = ""}) => {
     return (
-        <div>
-
-        </div>
+        <MainContainer>
+            <Avatar
+                large
+                username={name}
+            />
+            <Typography
+                variant="h4"
+                sx={{
+                    fontWeight: "bold",
+                    color: "white",
+                    marginLeft: "5px",
+                    marginRight: "5px",
+                }}
+            >
+                {name}
+            </Typography>
+        </MainContainer>
     );
 };
 
