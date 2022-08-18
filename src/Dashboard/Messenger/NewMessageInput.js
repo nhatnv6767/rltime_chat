34a663fb63177eb790d311a196ba6076d2863346
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 00:30:20 +0900

    #114 Creating new message input 2

--- src/Dashboard/Messenger/NewMessageInput.js
+++ src/Dashboard/Messenger/NewMessageInput.js
@@ -2,13 +2,35 @@ import React, {useState} from 'react';
 import {styled} from "@mui/material/styles";
 import {connect} from "react-redux";
 
+const MainContainer = styled("div")({
+    height: "60px",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+})
+
+const Input = styled("input")({
+    backgroundColor: "#2f3136",
+    height: "98%",
+    color: "white",
+    border: "none",
+    borderRadius: "8px",
+    fontSize: "14px",
+    padding: "0 10px",
+})
 
 const NewMessageInput = ({chosenChatDetails}) => {
     const [message, setMessage] = useState("")
     return (
-        <div>
-
-        </div>
+        <MainContainer>
+            <Input
+                placeholder={`Write message to ${chosenChatDetails.name}`}
+                value={message}
+                onChange={handleMessageValueChange}
+                onKeyDown={handleKeyPressed}
+            />
+        </MainContainer>
     );
 };
 
