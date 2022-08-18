5e0ccf65afb83a12aef387a4732c2a638d35d736
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 23:42:22 +0900

    #111 Creating Message Header 3

--- src/Dashboard/Messenger/Messages/Messages.js
+++ src/Dashboard/Messenger/Messages/Messages.js
@@ -3,12 +3,21 @@ import {styled} from "@mui/material/styles";
 import MessagesHeader from "./MessagesHeader"
 import {connect} from "react-redux"
 
+const MainContainer = styled("div")({
+    height: "calc(100% - 60px)",
+    overflow: "auto",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+})
 
 const Messages = ({chosenChatDetails, messages}) => {
     return (
-        <div>
-
-        </div>
+        <MainContainer>
+            <MessagesHeader
+                name={chosenChatDetails?.name}
+            />
+        </MainContainer>
     );
 };
 
