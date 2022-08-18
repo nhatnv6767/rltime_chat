f3c378ff4fe85535a4c1dee4ecfa1a36ffd9a3fd
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 10:50:59 +0900

    #90 Rendering real pending invitations list 3

--- src/store/store.js
+++ src/store/store.js
@@ -3,10 +3,12 @@ import {combineReducers, createStore, applyMiddleware} from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import authReducer from "./reducers/authReducer";
 import alertReducer from "./reducers/alertReducer";
+import friendsReducer from "./reducers/friendsReducer";
 
 const rootReducer = combineReducers({
     auth: authReducer,
     alert: alertReducer,
+    friendsReducer: friendsReducer,
 });
 
 const store = createStore(
