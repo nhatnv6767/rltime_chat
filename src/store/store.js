6f5d1a40382c47aee9ea8daef5892ec05997cc9c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 11:27:49 +0900

    #93 Testing and fix bug 4

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import {composeWithDevTools} from "redux-devtools-extension";
-import {combineReducers, createStore, applyMiddleware} from "@reduxjs/toolkit";
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from "redux-thunk";
 import authReducer from "./reducers/authReducer";
 import alertReducer from "./reducers/alertReducer";
