d68e571c0a702252bce8fea3ca27cbed069a9b74
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 23:15:51 +0900

    #141 Adding resize functionality to room 4

--- src/Dashboard/Room/ResizeRoomButton.js
+++ src/Dashboard/Room/ResizeRoomButton.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {styled} from "@mui/material/styles";
+import {IconButton} from "@mui/material";
+import CloseFullscreenIcon from "@mui/icons-material/CloseFullscreen";
+import OpenInFullIcon from "@mui/icons-material/OpenInFull"
 
 const MainContainer = styled("div")({
     position: "absolute",
@@ -11,7 +14,13 @@ const MainContainer = styled("div")({
 const ResizeRoomButton = ({isRoomMinimized, handleRoomResize}) => {
     return (
         <MainContainer>
-
+            <IconButton
+                style={{color: 'white'}}
+            >
+                {
+                    isRoomMinimized ? <OpenInFullIcon/> : <CloseFullscreenIcon/>
+                }
+            </IconButton>
         </MainContainer>
     );
 };
