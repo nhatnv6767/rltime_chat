5dc6d97f089d9a750b89c02b0f59cbd6d835d4cb
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-20 00:05:15 +0900

    #144 Creating Room 1

--- src/realtimeCommunication/roomHandler.js
+++ src/realtimeCommunication/roomHandler.js
@@ -1,6 +1,8 @@
 import store from "../store/store";
 import {setOpenRoom} from "../store/actions/roomActions";
+import * as socketConnection from "./socketConnection"
 
 export const createNewRoom = () => {
     store.dispatch(setOpenRoom(true, true));
+    socketConnection.createNewRoom();
 }
