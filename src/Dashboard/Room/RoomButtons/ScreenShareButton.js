6a70cc94b8eb95200f5ac2fb9fc25634577220ee
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 23:50:51 +0900

    #143 Creating room buttons 7

--- src/Dashboard/Room/RoomButtons/ScreenShareButton.js
+++ src/Dashboard/Room/RoomButtons/ScreenShareButton.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {styled} from "@mui/material/styles";
+import IconButton from '@mui/material/IconButton'
+import ScreenShareIcon from '@mui/icons-material/ScreenShare';
+import StopScreenShareIcon from '@mui/icons-material/StopScreenShare';
 
 const ScreenShareButton = () => {
-    return (
-        <div>
 
-        </div>
+    const [screenShareEnabled, setScreenShareEnabled] = useState(true);
+
+    const handleToggleScreenShare = () => {
+        setScreenShareEnabled(!screenShareEnabled);
+    }
+
+    return (
+        <IconButton
+            onClick={handleToggleScreenShare}
+            style={{color: 'white'}}
+        >
+            {
+                screenShareEnabled ? <ScreenShareIcon/> : <StopScreenShareIcon/>
+            }
+        </IconButton>
     );
 };
 
