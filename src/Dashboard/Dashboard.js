72b19b53daa0c510cc803dc6c0977a3da3b2b871
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 23:04:10 +0900

    #140 Create main room component 6

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -8,36 +8,40 @@ import {logout} from '../shared/utils/auth';
 import {connect} from 'react-redux';
 import {getActions} from '../store/actions/authActions';
 import {
-  connectWithSocketServer,
+    connectWithSocketServer,
 } from '../realtimeCommunication/socketConnection';
+import Room from "./Room/Room";
 
 const Wrapper = styled('div')({
-  width: '100%', height: '100vh', display: 'flex',
+    width: '100%', height: '100vh', display: 'flex',
 });
 
-const Dashboard = ({setUserDetails}) => {
-  useEffect(() => {
-    const userDetails = localStorage.getItem('user');
-    if (!userDetails) {
-      logout();
-    } else {
-      setUserDetails(JSON.parse(userDetails));
-      connectWithSocketServer(JSON.parse(userDetails));
-    }
-  }, []);
+const Dashboard = ({setUserDetails, isUserInRoom}) => {
+    useEffect(() => {
+        const userDetails = localStorage.getItem('user');
+        if (!userDetails) {
+            logout();
+        } else {
+            setUserDetails(JSON.parse(userDetails));
+            connectWithSocketServer(JSON.parse(userDetails));
+        }
+    }, []);
 
-  return (<Wrapper>
-    <SideBar/>
-    <FriendSideBar/>
-    <Messenger/>
-    <AppBar/>
-  </Wrapper>);
+    return (<Wrapper>
+        <SideBar/>
+        <FriendSideBar/>
+        <Messenger/>
+        <AppBar/>
+        {
+            isUserInRoom && <Room/>
+        }
+    </Wrapper>);
 };
 
 const mapActionsToProps = (dispatch) => {
-  return {
-    ...getActions(dispatch),
-  };
+    return {
+        ...getActions(dispatch),
+    };
 };
 
 export default connect(null, mapActionsToProps)(Dashboard);
