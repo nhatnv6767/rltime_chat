c017984322db79502968346c0487ad8a78c7ed5c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-20 01:47:21 +0900

    #149 Rendering active rooms 7

--- src/Dashboard/SideBar/ActiveRoomButton.js
+++ src/Dashboard/SideBar/ActiveRoomButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Tooltip} from "@mui/material";
 import Button from "@mui/material/Button";
+import Avatar from "../../shared/components/Avatar";
 
 const ActiveRoomButton = ({
                               creatorUsername,
@@ -24,9 +25,21 @@ const ActiveRoomButton = ({
         >
             <div>
                 <Button
+                    style={{
+                        width: "48px",
+                        height: "48px",
+                        borderRadius: "16px",
+                        margin: 0,
+                        padding: 0,
+                        minWidth: 0,
+                        marginTop: "10px",
+                        color: "white",
+                        backgroundColor: "#5865F2"
+                    }}
                     disabled={activeRoomButtonDisabled || isUserInRoom}
                     onClick={handleJoinActiveRoom}
                 >
+                    <Avatar username={creatorUsername}/>
                 </Button>
             </div>
         </Tooltip>
