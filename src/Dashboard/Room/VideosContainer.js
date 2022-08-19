dd9d174a76c18b3ed1e1ae1294160d42a9bc171b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 23:22:04 +0900

    #142 Creating structure of room dialog 2

--- src/Dashboard/Room/VideosContainer.js
+++ src/Dashboard/Room/VideosContainer.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {styled} from "@mui/material/styles";
+
+const MainContainer = styled("div")({
+    height: "85%",
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap"
+})
 
 const VideosContainer = () => {
     return (
-        <div>
+        <MainContainer>
 
-        </div>
+        </MainContainer>
     );
 };
 
