e276274eb487a42e1bd170553cd5e893d09fde87
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 23:47:34 +0900

    #143 Creating room buttons 6

--- src/Dashboard/Room/RoomButtons/MicButton.js
+++ src/Dashboard/Room/RoomButtons/MicButton.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {styled} from "@mui/material/styles";
+import IconButton from '@mui/material/IconButton'
+import MicIcon from "@mui/icons-material/Mic"
+import MicOffIcon from '@mui/icons-material/MicOff';
 
 const MicButton = () => {
-    return (
-        <div>
 
-        </div>
+    const [micEnabled, setMicEnabled] = useState(true);
+
+    const handleToggleMic = () => {
+        setMicEnabled(!micEnabled);
+    }
+
+    return (
+        <IconButton
+            onClick={handleToggleMic}
+            style={{color: 'white'}}
+        >
+            {
+                micEnabled ? <MicIcon/> : <MicOffIcon/>
+            }
+        </IconButton>
     );
 };
 
