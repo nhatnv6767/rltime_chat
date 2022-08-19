0f5e5cecf60782668c466ccd2a0634fd610369cd
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 23:23:53 +0900

    #142 Creating structure of room dialog 3

--- src/Dashboard/Room/RoomButtons.js
+++ src/Dashboard/Room/RoomButtons.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {styled} from "@mui/material/styles";
+
+const MainContainer = styled("div")({
+    height: "15%",
+    width: "100%",
+    backgroundColor: "#5865f2",
+    borderTopLeftRadius: "8px",
+    borderTopRightRadius: "8px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+})
 
 const RoomButtons = () => {
     return (
-        <div>
+        <MainContainer>
 
-        </div>
+        </MainContainer>
     );
 };
 
