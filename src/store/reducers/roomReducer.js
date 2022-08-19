af755a1216b51120b36d7b70a664bb82d57029f6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 20:46:07 +0900

    #137 <Group Call> Preparing Store state 1

---
+++ src/store/reducers/roomReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+    isUserInRoom: false,
+    isUserRoomCreator: false,
+    roomDetails: null,
+    activeRooms: [],
+    localStream: null,
+    remoteStreams: [],
+    audioOnly: false,
+    screenSharingStream: null,
+    isScreenSharingActive: false,
+}
