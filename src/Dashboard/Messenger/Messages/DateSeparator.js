f0c377eecc33c8f74349ef5f1c69ff69f941a7c4
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 19:44:24 +0900

    #127 Creating Date Separator 4

--- src/Dashboard/Messenger/Messages/DateSeparator.js
+++ src/Dashboard/Messenger/Messages/DateSeparator.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import {styled} from "@mui/material/styles";
+
+const Separator = styled("div")({
+    width: "95%",
+    backgroundColor: "#b9bbbe",
+    height: "1px",
+    position: "relative",
+    marginTop: "20px",
+    marginBottom: "10px",
+})
+
+const DateLabel = styled("span")({
+    backgroundColor: "#36393f",
+    position: "absolute",
+    left: "45%",
+    top: "-10px",
+    color: "#b9bbbe",
+    padding: "0 5px",
+    fontSize: "14px",
+})
 
 const DateSeparator = () => {
     return (
-        <div>
+        <Separator>
 
-        </div>
+        </Separator>
     );
 };
 
