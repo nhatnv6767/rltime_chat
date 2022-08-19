315c3f6047f4d0a678a65c04a79df5e8a37dd7d3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-20 02:08:23 +0900

    #150 Joining active room 2

--- src/realtimeCommunication/socketConnection.js
+++ src/realtimeCommunication/socketConnection.js
@@ -58,3 +58,7 @@ export const getDirectChatHistory = (data) => {
 export const createNewRoom = () => {
     socket.emit("room-create")
 }
+
+export const joinRoom = (data) => {
+    socket.emit("room-join", data)
+}
