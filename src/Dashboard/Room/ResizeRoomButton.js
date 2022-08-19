41158962145e04242d7ab09b0a2c67a833bb1838
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 23:09:46 +0900

    #141 Adding resize functionality to room 2

---
+++ src/Dashboard/Room/ResizeRoomButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResizeRoomButton = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ResizeRoomButton;
--- src/Dashboard/Room/Room.js
+++ src/Dashboard/Room/Room.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {styled} from "@mui/material/styles";
+import ResizeRoomButton from "./ResizeRoomButton";
 
 const MainContainer = styled("div")({
     position: "absolute",
