5dd8891ce2e0eec3460e0c8cffea7faf83d3d6a0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 22:05:20 +0900

    #137 <Group Call> Preparing Store state 5

--- src/Dashboard/SideBar/CreateRoomButton.js
+++ src/Dashboard/SideBar/CreateRoomButton.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add"
 
 const CreateRoomButton = () => {
-    return (
-        <div>
 
-        </div>
+    const createNewRoomHandler = () => {
+        // creating a room and sending infor to the server about this
+
+    }
+
+    return (
+        <Button
+            style={{
+                width: "48px",
+                height: "48px",
+                borderRadius: "16px",
+                margin: 0,
+                padding: 0,
+                minWidth: 0,
+                marginTop: "10px",
+                color: "white",
+                backgroundColor: "#5865F2"
+            }}
+        >
+            <AddIcon/>
+        </Button>
     );
 };
 
