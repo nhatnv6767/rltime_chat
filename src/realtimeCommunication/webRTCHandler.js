9c10e42fee90bfd6a72753e8542e1f9625b95a9a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 00:04:43 +0900

    #167 Preparing peer connection object 4

--- src/realtimeCommunication/webRTCHandler.js
+++ src/realtimeCommunication/webRTCHandler.js
@@ -2,6 +2,23 @@ import store from "../store/store";
 import {setLocalStream} from "../store/actions/roomActions";
 import Peer from "simple-peer";
 
+const getConfiguration = () => {
+    const turnIceServers = null;
+
+    if (turnIceServers) {
+        // TODO use TURN server credentials
+    } else {
+        console.warn("Using only STUN server")
+        return {
+            iceServers: [
+                {
+                    urls: "stun:stun.l.google.com:19302"
+                }
+            ]
+        }
+    }
+}
+
 const onlyAudioConstraints = {
     audio: true,
     video: false,
@@ -11,6 +28,7 @@ const defaultConstraints = {
     audio: true,
     video: true,
 }
+
 
 export const getLocalStreamPreview = (audioOnly = false, callbackFunc) => {
     const constraints = audioOnly ? onlyAudioConstraints : defaultConstraints;
