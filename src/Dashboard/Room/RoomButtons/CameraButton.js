5d6f99e6add2c17c09b4eb82b2f2b183e152c47d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 16:02:25 +0900

    #176 Connecting logic to video button 3

--- src/Dashboard/Room/RoomButtons/CameraButton.js
+++ src/Dashboard/Room/RoomButtons/CameraButton.js
@@ -3,11 +3,12 @@ import IconButton from '@mui/material/IconButton'
 import VideocamIcon from "@mui/icons-material/Videocam"
 import VideocamOffIcon from "@mui/icons-material/VideocamOff"
 
-const CameraButton = () => {
+const CameraButton = ({localStream}) => {
 
     const [cameraEnabled, setCameraEnabled] = useState(true);
 
     const handleToggleCamera = () => {
+        localStream.getVideoTracks()[0].enabled = !cameraEnabled;
         setCameraEnabled(!cameraEnabled);
     }
 
