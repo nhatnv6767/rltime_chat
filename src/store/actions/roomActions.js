769f74c6953cb68dadc6f32757744cf599c84b9a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 14:24:10 +0900

    #173 Rendering remote streams 3

--- src/store/actions/roomActions.js
+++ src/store/actions/roomActions.js
@@ -49,3 +49,10 @@ export const setAudioOnly = (audioOnly) => {
         audioOnly,
     }
 }
+
+export const setRemoteStreams = (remoteStreams) => {
+    return {
+        type: roomActions.SET_REMOTE_STREAMS,
+        remoteStreams,
+    }
+}
