b81da24dd01949cb36b22bdfe3a510cd8ba9bf68
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 16:06:10 +0900

    #177 Connecting logic to mute button 1

--- src/Dashboard/Room/RoomButtons/MicButton.js
+++ src/Dashboard/Room/RoomButtons/MicButton.js
@@ -3,11 +3,12 @@ import IconButton from '@mui/material/IconButton'
 import MicIcon from "@mui/icons-material/Mic"
 import MicOffIcon from '@mui/icons-material/MicOff';
 
-const MicButton = () => {
+const MicButton = ({localStream}) => {
 
     const [micEnabled, setMicEnabled] = useState(true);
 
     const handleToggleMic = () => {
+        localStream.getAudioTracks()[0].enabled = !micEnabled;
         setMicEnabled(!micEnabled);
     }
 
