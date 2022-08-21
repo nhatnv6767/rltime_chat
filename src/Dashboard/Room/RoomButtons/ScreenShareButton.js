7de15667ec8cc78f7f388aaf7054589aa919afae
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 16:22:17 +0900

    #178 Preparing logic for screen sharing 9

--- src/Dashboard/Room/RoomButtons/ScreenShareButton.js
+++ src/Dashboard/Room/RoomButtons/ScreenShareButton.js
@@ -3,6 +3,12 @@ import IconButton from '@mui/material/IconButton'
 import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare';
 import {setScreenSharingStream} from "../../../store/actions/roomActions";
+import video from "../Video";
+
+const constraints = {
+    audio: false,
+    video: true,
+}
 
 const ScreenShareButton = ({
                                localStream,
@@ -19,7 +25,7 @@ const ScreenShareButton = ({
             try {
                 stream = await navigator.mediaDevices.getDisplayMedia(constraints)
             } catch (err) {
-
+                console.log("Error occurred when trying to get an access to screen share stream")
             }
         }
     }
