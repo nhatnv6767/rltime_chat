1a7b726d0fefe3749c02f8340adf435e3e7473e0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 16:17:33 +0900

    #178 Preparing logic for screen sharing 7

--- src/Dashboard/Room/RoomButtons/ScreenShareButton.js
+++ src/Dashboard/Room/RoomButtons/ScreenShareButton.js
@@ -2,8 +2,14 @@ import React, {useState} from 'react';
 import IconButton from '@mui/material/IconButton'
 import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare';
+import {setScreenSharingStream} from "../../../store/actions/roomActions";
 
-const ScreenShareButton = () => {
+const ScreenShareButton = ({
+                               localStream,
+                               screenSharingStream,
+                               setScreenSharingStream,
+                               isScreenSharingActive,
+                           }) => {
 
     const [isScreenSharingActive, setIsScreenSharingActive] = useState(false);
 
